af92595e28527e7bf0d2fbebac406f991c776292
surajthenx <suraj994990@gmail.com>
2023-06-28 22:04:24 +0530

    first

---
+++ B-64/src/first.java
@@ -0,0 +1,5 @@
+public class first {
+    public static void main(String[] args){
+        System.out.println("welcome to new way of coding where you can have your codes all the places you go");
+    }
+}
